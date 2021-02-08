aaa9ae9110f11dceb7783d99681b26f0e1972ba3
MarijanaNicic <MarijanaNicic@users.noreply.github.com>
2021-02-08 13:57:50 +0100

    SetCapability

--- src/main/java/ExampleInstallAPK.java
+++ src/main/java/ExampleInstallAPK.java
@@ -11,14 +11,6 @@ public class ExampleInstallAPK extends SetCapability {
 
     DesiredCapabilities dc= new DesiredCapabilities();
 
-    //dc.setCapability(MobileCapabilityType.AUTOMATION_NAME,"Appium");
-
-    //dc.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-
-    //dc.setCapability(MobileCapabilityType.PLATFORM_VERSION,"7.0");
-
-    //dc.setCapability(MobileCapabilityType.DEVICE_NAME,"WUJ01RXT46");
-
     dc.setCapability(MobileCapabilityType.APP, "C:/Users/PC/Downloads/Blic_v3.1.3299_apkpure.com.apk");
 
     URL url = new URL("http://127.0.0.1:4723/wd/hub");
